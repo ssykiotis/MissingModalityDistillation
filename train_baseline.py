a1095f05e545c530a6a217bde9b662f1840c2052
Stavros Sykiotis <stasykiotis@gmail.com>
2024-02-01 15:47:58 +0200

    initial commit

---
+++ train_baseline.py
@@ -0,0 +1,25 @@
+import hydra
+import os
+import logging
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import DictConfig, OmegaConf
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+@hydra.main(version_base = "1.3", config_path = "config", config_name = "main_config")
+def main(config: DictConfig) -> None:
+    if config.device  == 'cuda':
+        if not torch.cuda.is_available():
+            logging.info('GPU not available: switching to CPU')
+            config.device = 'cpu'
+
+    setup_seed(config.seed)
+
+    ds_parser = #hydra.utils.instantiate dataset_parser
+    trainer   = Trainer(config, ds_parser)
+
+    trainer.train()
