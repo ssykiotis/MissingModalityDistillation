7eeeb6f63a9557f57f7f2ef393f01d673c0317c3
Stavros Sykiotis <stasykiotis@gmail.com>
2024-02-08 12:57:58 +0200

    added helpers file

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+import random
+
+def setup_seed(seed: int):
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark     = False  
+    random.seed(seed)                          
+    np.random.seed(seed)                       
+    torch.manual_seed(seed)                    
+    torch.cuda.manual_seed(seed)               
+    torch.cuda.manual_seed_all(seed) 
