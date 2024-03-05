733eea8558de15c2d248f48c4bb6ff7dcd457332
Stavros Sykiotis <stasykiotis@gmail.com>
2024-03-05 18:09:33 +0200

    added get_dataloader function

--- src/training/Trainer.py
+++ src/training/Trainer.py
@@ -5,6 +5,7 @@ import hydra
 
 from omegaconf import DictConfig
 from src.dataset.DatasetParser import *
+import torch.utils.data as data_utils
 
 class Trainer:
 
@@ -17,12 +18,14 @@ class Trainer:
         self.initial_lr = config.lr
         self.loss_fn    = hydra.utils.instantiate(config.loss)
         self.optimizer  = None
+
+        self.train_dl = self.get_dataloader('train')
+        self.val_dl   = self.get_dataloader('val')
         
         pass
 
     #TODO
     def train(self):
-        self.model.train()
         for epoch in range(self.epochs):
             curr_lr = self.intial_lr * (1.0-np.float32(epoch)/np.float32(self.epochs))**(0.9)
             self.train_one_epoch(curr_lr)
@@ -30,6 +33,7 @@ class Trainer:
         pass
     #TODO
     def train_one_epoch(self,lr):
+        self.model.train()
         for parm in self.optimizer.param_groups:
             parm['lr'] = lr
         loss_values = []
@@ -42,4 +46,26 @@ class Trainer:
 
     #TODO
     def test():
+        test_dl = self.get_dataloader('test')
         pass
+
+    def get_dataloader(self,mode: str):
+        dataset = self.ds_parser.get_dataset(mode)
+        self.norm_params = dataset.norm_params
+
+        if mode == "train":
+
+            dataloader = data_utils.Dataloader(dataset,
+                                               batch_size = self.config.batch_size,
+                                               shuffle    = True,
+                                               pin_memory = True,
+                                               drop_last  = False
+                                               )
+        else:
+            dataloader = data_utils.Dataloader(dataset,
+                                        batch_size = self.config.batch_size,
+                                        shuffle    = False,
+                                        pin_memory = True,
+                                        drop_last  = False
+                                        )
+        return dataloader
