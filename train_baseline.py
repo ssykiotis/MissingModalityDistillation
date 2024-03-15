fdeb7f28782ee34a2e6ef523e41535d678f056ca
Stavros Sykiotis <stasykiotis@gmail.com>
2024-03-15 15:34:45 +0200

    updated train_baseline scripts

--- train_baseline.py
+++ train_baseline.py
@@ -3,6 +3,8 @@ import os
 import logging
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
+from src.utils import *
+from src.training.Trainer import *
 
 
 
@@ -10,7 +12,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-@hydra.main(version_base = "1.3", config_path = "config", config_name = "main_config")
+@hydra.main(version_base = "1.3", config_path = "config", config_name = "train_baseline_config")
 def main(config: DictConfig) -> None:
     if config.device  == 'cuda':
         if not torch.cuda.is_available():
@@ -18,8 +20,17 @@ def main(config: DictConfig) -> None:
             config.device = 'cpu'
 
     setup_seed(config.seed)
+    
+    config['log_location'] = HydraConfig.get().runtime.output_dir
 
-    ds_parser = #hydra.utils.instantiate dataset_parser
+    ds_parser = hydra.utils.instantiate(config.dataset)
+    # dataset   = ds_parser.get_dataset('train',config.training_mode)
     trainer   = Trainer(config, ds_parser)
 
     trainer.train()
+    trainer.test()
+
+
+
+if __name__=="__main__":
+    main()
