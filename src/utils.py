c26628a29006c2f88cbac5ad294d2000e2d22470
Stavros Sykiotis <stasykiotis@gmail.com>
2024-06-02 13:07:48 +0300

    added histogram equalization function for visualizations

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,17 @@ def setup_seed(seed: int):
     torch.manual_seed(seed)                    
     torch.cuda.manual_seed(seed)               
     torch.cuda.manual_seed_all(seed) 
+
+
+def image_histogram_equalization(image, number_bins=256):
+    # from http://www.janeriksolem.net/histogram-equalization-with-python-and.html
+
+    # get image histogram
+    image_histogram, bins = np.histogram(image.flatten(), number_bins, density=True)
+    cdf = image_histogram.cumsum() # cumulative distribution function
+    cdf = (number_bins-1) * cdf / cdf[-1] # normalize
+
+    # use linear interpolation of cdf to find new pixel values
+    image_equalized = np.interp(image.flatten(), bins[:-1], cdf)
+
+    return image_equalized.reshape(image.shape), cdf
